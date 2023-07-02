2264f717dc7f9e33430aae3518bfecd0f9f9877c
yoshiki YANO <y4yanosince2023@gmail.com>
2023-07-02 17:38:39 +0900

    init commit

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+docker build -t torch_tutorial .
+
---
+++ docker/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker run \
+ --net=host \
+ --gpus all\
+ -v /tmp/.X11-unix:/tmp/.X11-unix
+ -v /etc/localtime:/etc/localtime:ro \
+ -v $PWD/../1: root/1
+ -v $PWD/../2: root/2
+ -v $PWD/../3: root/3
+ -e DISPLAY=$DISPLAY \
+ -e NVIDIA_VISIBLE_DEVICES=all \
+ -e NVIDIA_DRIVER_CAPABILITIES=all \
+ -it --name "torch_tutorial_Y-I" torch_tutorial_Y-I \
+
+
